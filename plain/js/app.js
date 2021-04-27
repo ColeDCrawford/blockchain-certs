7d86c1cc3b784391f8965c091d2af636b00a421f
Cole Crawford <cole_crawford@harvard.edu>
2021-04-27 16:04:26 -0400

    Lots of updates
    - Update functions to move from jQuery 1.12 to 3.x
    - Update to Bootstrap4
    - Use app-bundle.js (need to bundle with browserify to be able to use the truffle-contract npm package)
    - Add a store of data in app.js
    - Remove boilerplate
    - Move loading data to functions
    - Start adding admin-only functionality

--- plain/js/app.js
+++ plain/js/app.js
@@ -2,25 +2,9 @@ var contract = require("@truffle/contract");
 App = {
   web3Provider: null,
   contracts: {},
+  store:{},
 
   init: async function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
-
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-        petsRow.append(petTemplate.html());
-      }
-    });
-
     return await App.initWeb3();
   },
 
@@ -61,80 +45,194 @@ App = {
     
       // Set the provider for our contract
       App.contracts.CertificateIssuer.setProvider(App.web3Provider);
+      
+      App.setAccount();
     
-      // Use our contract to retrieve and mark the adopted pets
-      // return App.markAdopted();
-      return App.getAllCourses();
+      return App.loadContractData();
     });
 
     return App.bindEvents();
   },
 
-  getAllCourses: function(){
-    console.log("getting all courses");
+  loadContractData: function(){
     var certIssuerInstance;
     App.contracts.CertificateIssuer.deployed().then(function(instance){
       certIssuerInstance = instance;
       console.log(certIssuerInstance);
 
-      let owner = certIssuerInstance.owner.call();
-      console.log(owner);
-      let issuerName = certIssuerInstance.issuerName.call();
-      console.log(issuerName);
-      let courseId = 1;
-      let course = certIssuerInstance.courses.call(courseId);
-      console.log(course);
-      let courseIds = certIssuerInstance.getAllCourseIds.call();
-      console.log(courseIds);
-      
-      let courses = certIssuerInstance.getAllCourses.call();
-      console.log(courses);
+      certIssuerInstance.owner.call().then(function(data){
+        App.store.ownerAddress = data;
+      })
+      certIssuerInstance.issuerName.call().then(function(data){
+        App.store.issuerName = data;
+      });
 
+      // Larger loads
+      App.loadCourseData();
+      App.loadStudentData();
+      App.loadCertificateTypeData();
+      // certIssuerInstance.getAllCertTypes.call().then(function(data){
+      //   App.store.certificateTypes = data;
+      // });
+      // certIssuerInstance.getAllStudents.call().then(function(data){
+      //   App.store.students = data;
+      // })
+
+      // Not actually using this - is there a better way to make this function wait for all the async calls to contract to load?
       return certIssuerInstance.getAllCourses.call();
     }).then(function(courses){
-      console.log(courses);
-      courses.forEach(function(c){
-        console.log(c);
+      // Populate with contract data
+      console.log('populating templates');
+
+      $('#ownerAddress').text(App.store.ownerAddress);
+      $('#issuerName').text(App.store.issuerName);
+
+      console.log(App.isOwner());
+      console.log(App.store.ownerAddress);
+      console.log(App.store.userAccount);
+      if(App.isOwner()){
+        $('.ownerOnly').toggle();
+      }
+
+    });
+  },
+
+  loadCourseData: function(){
+    var certIssuerInstance;
+    App.contracts.CertificateIssuer.deployed().then(function(instance){
+      certIssuerInstance = instance;
+      certIssuerInstance.getAllCourses.call().then(function(data){
+        App.store.courses = data;
+        $('#courses').empty();
+        App.store.courses.forEach(function(c){
+          var courseTemplate = $('#courseTemplate');
+          console.log(c);
+          courseTemplate.find('.courseName').text(c.courseName);
+          courseTemplate.find('.courseId').text(c.courseId);
+          courseTemplate.find('.courseDescription').text(c.courseDescription);
+          $('#courses').append(courseTemplate.html());
+        })
+      });
+    });
+  },
+
+  loadStudentData: function(){
+    App.contracts.CertificateIssuer.deployed().then(function(instance){
+      certIssuerInstance = instance;
+      certIssuerInstance.getAllStudents.call().then(function(data){
+        App.store.students = data;
+
+        // Wipe current data, then rebuild
+        $('#students').empty();
+
+        // Verify owner
+        App.store.students.forEach(function(s){
+          var studentTemplate = $('#studentTemplate');
+          studentTemplate.find('.studentName').text(s.name);
+          studentTemplate.find('.studentAddress').text(s.address);
+          $('#students').append(studentTemplate.html());
+        })
       })
     });
   },
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+  loadCertificateTypeData: function(){
+    App.contracts.CertificateIssuer.deployed().then(function(instance){
+      certIssuerInstance = instance;
+      certIssuerInstance.getAllCertTypes.call().then(function(data){
+        App.store.certificateTypes = data;
+        $('#certificateTypes').empty();
+        App.store.certificateTypes.forEach(function(t){
+          var certTypeTemplate = $('#certificateTypeTemplate');
+          console.log(t);
+          certTypeTemplate.find('.certificateName').text(t.certificateName);
+          certTypeTemplate.find('.certificateDescription').text(t.certificateDescription);
+          certTypeTemplate.find('.certificateTypeId').text(t.certificateTypeId);
+          certTypeTemplate.find('.numCoursesRequired').text(t.numCoursesRequired);
+          $('#certificateTypes').append(certTypeTemplate.html());
+        })
+      })
+    })
   },
 
-  markAdopted: function() {
-    var adoptionInstance;
+  bindEvents: function() {
+    $(document).on('click', '#addCourse', App.handleAddCourse);
+    $(document).on('click', '#addStudent', App.handleAddStudent);
+    $(document).on('click', '#addCertType', App.handleAddCertificateType);
+  },
 
-    App.contracts.Adoption.deployed().then(function(instance) {
-      adoptionInstance = instance;
-
-      return adoptionInstance.getAdopters.call();
-    }).then(function(adopters) {
-      for (i = 0; i < adopters.length; i++) {
-        if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
-          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
-        }
-      }
-    }).catch(function(err) {
+  addCourse: function(name, desc){
+    var certIssuerInstance;
+    App.contracts.CertificateIssuer.deployed().then(function(instance){
+      certIssuerInstance = instance;
+      return certIssuerInstance.addCourse(name, desc, {from: App.store.userAccount});
+    }).then(function(tx){
+      // could try to just add the new one?
+      console.log(tx);
+      App.loadCourseData();
+    }).catch(function(err){
       console.log(err.message);
     });
   },
 
-  handleAdopt: function(event) {
+  addStudent: function(address, name){
+    App.contracts.CertificateIssuer.deployed().then(function(instance){
+      // Need to see if the contract owner is the sender?
+      certIssuerInstance = instance;
+      return certIssuerInstance.addStudent(address, name, {from: App.store.userAccount});
+    }).then(function(tx){
+      console.log(tx);
+      App.loadStudentData();
+    });
+  },
+
+  addCertificateType: function(name, desc, numCoursesRequired){
+    App.contracts.CertificateIssuer.deployed().then(function(instance){
+      certIssuerInstance = instance;
+      return certIssuerInstance.addCertificateType(name, desc, numCoursesRequired, {from: App.store.userAccount});
+    }).then(function(tx){
+      console.log(tx);
+      App.loadCertificateTypeData();
+    })
+  },
+
+  setAccount: function(){
+    web3.eth.getAccounts().then(function(accounts){
+      App.store.userAccount = accounts[0];
+      return accounts[0];
+    });
+  },
+
+  handleAddCourse: function(event){
     event.preventDefault();
+    let name = $('#courseNameInput').val();
+    let description = $('#courseDescInput').val();
+    App.addCourse(name, description);
+  },
 
-    var petId = parseInt($(event.target).data('id'));
+  handleAddStudent: function(event){
+    event.preventDefault();
+    let name = $('#studentNameInput').val();
+    let address = $('#studentAddressInput').val();
+    App.addStudent(address, name);
+  },
 
-    /*
-     * Replace me...
-     */
+  handleAddCertificateType: function(event){
+    event.preventDefault();
+    let name = $('#certTypeNameInput').val();
+    let desc = $('#certTypeDescInput').val();
+    let numCoursesRequired = parseInt($('#certTypeNumCoursesRequired').val());
+    App.addCertificateType(name, desc, numCoursesRequired);
+  },
+
+  isOwner: function(){
+    return App.store.ownerAddress == App.store.userAccount;
   }
 
 };
 
 $(function() {
-  $(window).load(function() {
+  $(window).on('load', function() {
     App.init();
   });
 });
